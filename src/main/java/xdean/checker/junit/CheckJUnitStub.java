372ac2621cf58e5442274b5ca06249ab236c701a
XDean <373216024@qq.com>
2018-05-22 11:17:18 +0800

    add stub

---
+++ src/main/java/xdean/checker/junit/CheckJUnitStub.java
@@ -0,0 +1,11 @@
+package xdean.checker.junit;
+
+/**
+ * This is a stub file to avoid javadoc throw "No public or protected classes
+ * found to document"
+ *
+ * @author Dean Xu (XDean@github.com)
+ */
+public interface CheckJUnitStub {
+
+}
---
+++ src/main/java/xdean/checker/junit/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Check definition for JUnit annotations.
+ *
+ * @author Dean Xu (XDean@github.com)
+ */
+package xdean.checker.junit;
